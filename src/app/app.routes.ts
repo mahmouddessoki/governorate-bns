b81996354f3ae853341ec2bd1748d8c4ffd00886
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-08-19 01:24:05 +0300

    initialization commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { UserLayoutComponent } from './core/layouts/user-layout/user-layout.component';
+import { HomeComponent } from './features/home/home.component';
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/authentication/components/login/login.component';
+
+export const routes: Routes = [
+
+  {
+    path:'',
+    component:UserLayoutComponent,
+    children:[
+      {path:'home',component:HomeComponent},
+      {path:'',redirectTo:'home',pathMatch:'full'}
+    ]
+  },
+  {
+    path:'',
+    component:AuthLayoutComponent,
+    children:[
+      {path:'home',component:LoginComponent},
+      {path:'',redirectTo:'login',pathMatch:'full'}
+    ]
+  }
+
+
+
+
+];
