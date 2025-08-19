4dfebae470b460797d16314e408a2406bd640633
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-08-19 18:48:49 +0300

    replace bootstrap with tailwindcss and flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,afterRender } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'governorate';
+
+  constructor(){
+    afterRender(()=>{
+        initFlowbite(); // reinitialize Flowbite components
+    })
+  }
 }
