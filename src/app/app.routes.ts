a7748f4d908f631c762ad40aaf36170ac49582ca
Taha Mahmoud <taha.mahmoud@uaedigitallab.com>
2025-09-05 20:04:19 +0300

    update sidebar, added activity log

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,31 +5,34 @@ import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.comp
 import { LoginComponent } from './features/authentication/components/login/login.component';
 import { InformationNoteComponent } from './features/information-note/information-note.component';
 import { SectorsComponent } from './features/sectors/sectors.component';
+import { ActivityLogComponent } from './features/activity-log/activity-log.component';
 
 export const routes: Routes = [
   {
-    path:'',
-    component:UserLayoutComponent,
-    children:[
-      {path:'home',component:HomeComponent,title:'الصفحة الرئيسية'},
-      {path:'informative-note' , component:InformationNoteComponent,title:'النوتة التعريفية'},
-      {path:'sectors' , component:SectorsComponent,title:'القطاعات'},
-      {path:'',redirectTo:'home',pathMatch:'full'}
-    ]
+    path: '',
+    component: UserLayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent, title: 'الصفحة الرئيسية' },
+      {
+        path: 'informative-note',
+        component: InformationNoteComponent,
+        title: 'النوتة التعريفية',
+      },
+      { path: 'sectors', component: SectorsComponent, title: 'القطاعات' },
+      {
+        path: 'activity',
+        component: ActivityLogComponent,
+        title: 'سجل النشاط',
+      },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
   },
-   {
-    path:'',
-    component:AuthLayoutComponent,
-    children:[
-      {path:'login',component:LoginComponent,title:'Login'},
-      {path:'',redirectTo:'login',pathMatch:'full'}
-    ]
-  }
-,
-
-
-
-
-
-
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent, title: 'Login' },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
 ];
--- src/app/shared/components/ui/card/card.component.ts
+++ src/app/shared/components/ui/card/card.component.ts
@@ -1,10 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
+  link = input<string>('');
+
+  constructor(private router: Router) {
+    effect(() => {
+      console.log(this.link(), 'aaaa');
+    });
+  }
+
+  activity() {
+    this.router.navigate(['/activity'], { queryParams: { id: 3 } });
+  }
 }
