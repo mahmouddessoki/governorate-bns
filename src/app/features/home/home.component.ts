9f31438403421631d0b2f91f7c14b0599a8ea2f6
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-11-11 23:00:02 +0200

    nn

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CardComponent } from '../../shared/components/ui/card/card.component';
 import { SectionHeaderComponent } from '../../shared/components/ui/section-header/section-header.component';
 import { Card } from '../../shared/models/card';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [CardComponent, SectionHeaderComponent],
+  imports: [CardComponent, SectionHeaderComponent , NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
